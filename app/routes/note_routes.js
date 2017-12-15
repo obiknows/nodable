8e876982c114303067209f161801b1e552d73c15
obiknows <sam.nnodim@gmail.com>
2017-12-14 18:32:37 -0800

    even more functionality whoaaa.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -2,7 +2,7 @@ module.exports = function(app, db) {
 
   // a  post to notes
   app.post('/notes', (req, res) => {
-
+    console.log(req.body)
     // create the note here
     res.send('Hello')
   })
